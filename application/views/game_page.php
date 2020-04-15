57cef83a397cb1b48bd273245ca302a6b704fd6b
Leomam <Leomam@gmail.com>
2020-04-15 18:13:18 +0200

    gamepage

--- application/views/game_page.php
+++ application/views/game_page.php
@@ -1,12 +1,12 @@
 <div class="h-100 container-fluid">
-	<div class=" h-100 row align-middle">
-		<div class="card text-white bg-primary col-12">
+	<div class="h-100 row align-middle">
+		<div class="card text-center text-white bg-primary col-12">
 			<div class="card-header">
 			<h4>Gage n°12</h4>
 			</div>
 			<div class="card-body">
 				<h1 class="card-title">Primary card title</h1>
-				<h2 class="card-text"><?php echo "Gage : ". $gage . " Pour : " .$personne ?></h2>
+				<h2 class="card-text align-middle"><?php echo "Gage : ". $gage . " Pour : " .$personne ?></h2>
 			</div>
 			<div class="card-footer">
 				<h5>Tag : Alcool, Sports</h5>
