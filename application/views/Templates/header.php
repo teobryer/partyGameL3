ef83ace7341236ffd8dcf7fb3156355e03bf456f
Leomam <Leomam@gmail.com>
2020-04-15 16:52:53 +0200

    title header

--- application/views/Templates/header.php
+++ application/views/Templates/header.php
@@ -7,7 +7,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <head>
 	<meta charset="utf-8">
-	<title>Party Game - <?php echo $title ?>
+	<title>Party Game 
+		<?php if (isset($title))
+			echo " - $title";
+		?>
 	</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
 		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
