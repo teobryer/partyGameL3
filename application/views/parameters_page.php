35874bd4310a78fc213ebe6d92ced94b0494ef11
Leomam <axel.froly@gmail.com>
2020-04-16 18:46:20 +0200

    alcool checkbox layout

--- application/views/parameters_page.php
+++ application/views/parameters_page.php
@@ -5,6 +5,7 @@
 				<h1>Game Parameters</h1>
 			</div>
 			<div class="card-body container">
+				<!-- Input Personne -->
 				<div class="container row mx-auto mb-5">
 					<div class="btn-group col-lg-2 col-xl-2 col-md-2 ml-auto" role="group">
 						<button type="button" class="btn btn-secondary">♂</button>
@@ -17,30 +18,59 @@
 						<button type="button" class="btn btn-info">+</button>
 					</div>
 				</div>
-				<div class="container row mx-auto mt-2">
-					<div class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 ml-auto" role="group">
-						<button type="button" class="close col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4" aria-label="Close">
+				<!-- Titles -->
+				<div class="container row col-12 mx-auto mb-2">
+					<div class="col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8">
+						<!-- Empty Layout Div -->
+					</div>
+					<p class="col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4">Alcool</p>
+				</div>
+				<!-- Personne 1 -->
+				<div class="container row mx-auto mb-2">
+					<div class="btn-group col-lg-11 col-xl-11 col-md-11 col-sm-11 col-10 ml-auto" role="group">
+						<button type="button" class="close col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4"
+							aria-label="Close">
 							<span aria-hidden="true">&times;</span>
 						</button>
-						<h3 class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 card-text align-text-bottom" role="group">
-						Joe
+						<h3 class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 card-text align-text-bottom"
+							role="group">
+							Joe
 						</h3>
 					</div>
-					<div class="btn-group col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4 mr-auto" role="group">
-						<button type="button" class="btn btn-info">+</button>
+					<div class="btn-group col-lg-1 col-xl-1 col-md-1 col-sm-1 col-2 mr-auto" role="group">
+						<input class="form-check-input" type="checkbox" value="" id="AlcoolCheckbox" checked>
 					</div>
 				</div>
-				<div class="container row mx-auto mt-2">
-					<div class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 ml-auto" role="group">
-						<button type="button" class="close col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4" aria-label="Close">
+				<!-- Personne 2 -->
+				<div class="container row mx-auto mb-2">
+					<div class="btn-group col-lg-11 col-xl-11 col-md-11 col-sm-11 col-10 ml-auto" role="group">
+						<button type="button" class="close col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4"
+							aria-label="Close">
 							<span aria-hidden="true">&times;</span>
 						</button>
-						<h3 class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 card-text align-text-bottom" role="group">
-						Lucie
+						<h3 class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 card-text align-text-bottom"
+							role="group">
+							Lucie
 						</h3>
 					</div>
-					<div class="btn-group col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4 mr-auto" role="group">
-						<button type="button" class="btn btn-info">+</button>
+					<div class="btn-group col-lg-1 col-xl-1 col-md-1 col-sm-1 col-2 mr-auto" role="group">
+						<input class="form-check-input" type="checkbox" value="" id="AlcoolCheckbox">
+					</div>
+				</div>
+				<!-- Personne 3 -->
+				<div class="container row mx-auto mb-2">
+					<div class="btn-group col-lg-11 col-xl-11 col-md-11 col-sm-11 col-10 ml-auto" role="group">
+						<button type="button" class="close col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4"
+							aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h3 class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 card-text align-text-bottom"
+							role="group">
+							Mathieu
+						</h3>
+					</div>
+					<div class="btn-group col-lg-1 col-xl-1 col-md-1 col-sm-1 col-2 mr-auto" role="group">
+						<input class="form-check-input" type="checkbox" value="" id="AlcoolCheckbox" checked>
 					</div>
 				</div>
 			</div>
