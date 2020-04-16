f6b85824771d912d7983fc6aa88c080bc360a638
Leomam <Leomam@gmail.com>
2020-04-16 17:35:10 +0200

    player's name good

--- application/views/parameters_page.php
+++ application/views/parameters_page.php
@@ -2,19 +2,44 @@
 	<div class="h-100 row">
 		<div class="card text-center text-white bg-primary col-12">
 			<div class="card-header">
-				<h1>Parameters</h1>
+				<h1>Game Parameters</h1>
 			</div>
 			<div class="card-body container">
-				<div class="container row mx-auto">
+				<div class="container row mx-auto mb-5">
 					<div class="btn-group col-lg-2 col-xl-2 col-md-2 ml-auto" role="group">
 						<button type="button" class="btn btn-secondary">♂</button>
 						<button type="button" class="btn btn-secondary">♀</button>
 					</div>
 					<div class="btn-group col-lg-8 col-xl-8 col-md-8" role="group">
-						<input type="text" class="form-control" id="usernameInput"
-							placeholder="username">
+						<input type="text" class="form-control" id="usernameInput" placeholder="username">
 					</div>
 					<div class="btn-group col-lg-2 col-xl-2 col-md-2 mr-auto" role="group">
+						<button type="button" class="btn btn-info">+</button>
+					</div>
+				</div>
+				<div class="container row mx-auto mt-2">
+					<div class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 ml-auto" role="group">
+						<button type="button" class="close col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h3 class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 card-text align-text-bottom" role="group">
+						Joe
+						</h3>
+					</div>
+					<div class="btn-group col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4 mr-auto" role="group">
+						<button type="button" class="btn btn-info">+</button>
+					</div>
+				</div>
+				<div class="container row mx-auto mt-2">
+					<div class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 ml-auto" role="group">
+						<button type="button" class="close col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h3 class="btn-group col-lg-10 col-xl-10 col-md-10 col-sm-10 col-8 card-text align-text-bottom" role="group">
+						Lucie
+						</h3>
+					</div>
+					<div class="btn-group col-lg-2 col-xl-2 col-md-2 col-sm-2 col-4 mr-auto" role="group">
 						<button type="button" class="btn btn-info">+</button>
 					</div>
 				</div>
