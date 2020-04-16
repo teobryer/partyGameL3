67c135babcd3d18f0fed4057229cb1631b3f1a08
Leomam <Leomam@gmail.com>
2020-04-16 16:44:34 +0200

    parameter page responsive buttons

--- application/views/parameters_page.php
+++ application/views/parameters_page.php
@@ -5,16 +5,16 @@
 				<h1>Parameters</h1>
 			</div>
 			<div class="card-body container">
-				<div class="container row">
-					<div class="btn-group col-1 ml-auto" role="group" aria-label="Basic example">
+				<div class="container row mx-auto">
+					<div class="btn-group col-lg-2 col-xl-2 col-md-2 ml-auto" role="group">
 						<button type="button" class="btn btn-secondary">♂</button>
 						<button type="button" class="btn btn-secondary">♀</button>
 					</div>
-					<div class="btn-group col-3" role="group" aria-label="Basic example">
+					<div class="btn-group col-lg-8 col-xl-8 col-md-8" role="group">
 						<input type="text" class="form-control" id="usernameInput"
 							placeholder="username">
 					</div>
-					<div class="btn-group col-1 mr-auto" role="group" aria-label="Basic example">
+					<div class="btn-group col-lg-2 col-xl-2 col-md-2 mr-auto" role="group">
 						<button type="button" class="btn btn-info">+</button>
 					</div>
 				</div>
