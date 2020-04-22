0b150396987aac9d987a31b4e7b1af8572f31d74
Leomam <axel.froly@gmail.com>
2020-04-22 16:16:19 +0200

    color homepage

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -4,7 +4,7 @@
 			<div class="col my-auto" style="text-align:center">
 				<a href='<?php base_url() ?>game'>
 					<svg width="310" height="310">
-						<circle cx="155" cy="155" r="150" fill="blue" />
+						<circle cx="155" cy="155" r="150" fill="#007bff" />
 						<text x="50%" y="50%" text-anchor="middle" fill="white" font-size="25px" font-family="Arial"
 							dy=".3em">Partie rapide</text>
 						Sorry, your browser does not support inline SVG.
@@ -15,7 +15,7 @@
 			<div class="col my-auto" style="text-align:center">
 				<a href='<?php base_url() ?>parameters'>
 					<svg width="310" height="310">
-						<circle cx="155" cy="155" r="150" fill="red"/> <!--onclick="alert('Partie perso')"-->
+						<circle cx="155" cy="155" r="150" fill="#dc3545"/> <!--onclick="alert('Partie perso')"-->
 						<text x="50%" y="50%" text-anchor="middle" fill="white" font-size="25px" font-family="Arial"
 							dy=".3em">Partie personnalis&eacutee</text>
 						Sorry, your browser does not support inline SVG.
@@ -26,7 +26,7 @@
 			<div class="col my-auto" style="text-align:center">
         <a href='<?php base_url() ?>parameters'>
           <svg width="310" height="310">
-            <circle cx="155" cy="155" r="150" fill="green"/> <!--onclick="alert('Partie à distance')"-->
+            <circle cx="155" cy="155" r="150" fill="#28a745"/> <!--onclick="alert('Partie à distance')"-->
             <text x="50%" y="50%" text-anchor="middle" fill="white" font-size="25px" font-family="Arial" dy=".3em">Partie
               &agrave distance</text>
             Sorry, your browser does not support inline SVG.
