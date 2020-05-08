408bacd6aa557239404c8e0ec1612936fd051daf
Leomam <axel.froly@gmail.com>
2020-05-08 15:34:03 +0200

    good base

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -6,10 +6,10 @@ class Account extends CI_Controller {
 	public function __construct ()
     {
     	parent::__construct();
-        //$this->load->model('personne_model');
-        //$this->load->helper('url');
-        //$this->load->library('session');
-        //$this->session->unset_userdata('instancePartie');
+        $this->load->model('personne_model');
+        $this->load->helper('url');
+        $this->load->library('session');
+        $this->session->unset_userdata('instancePartie');
     }   
     
     
